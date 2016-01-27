95e7b746e14feed87e9511d3a1e0d046c6f59914
secret_online <me+github@secretonline.co>
2016-01-27 14:01:56 +1300

    Add proper generation function

--- scripts/generator.js
+++ scripts/generator.js
@@ -12,7 +12,50 @@
    * @return {string} Planet name
    */
   function generatePlanetName() {
-    return '[Error]: Generator not written yet';
+    var name;
+    var form = doc.querySelector('.form-planetName');
+    var sP = form.querySelector('input[name=planet-solarPrefix]').value;
+    var sI = form.querySelector('input[name=planet-solarIndex]').value;
+    var sI2 = form.querySelector('input[name=planet-solarIndexBinary]').value;
+    var pSize = form.querySelector('input[name=planet-size]').value.toString();
+    var pClimate = form.querySelector('input[name=planet-climate]').value.toString();
+    var pSurv = form.querySelector('input[name=planet-survivability]').value.toString();
+    var mods = form.querySelectorAll('.planet-suffix input[type=checkbox]');
+
+    if (!sI) {
+      return '[Error]: No Solar Index was given';
+    }
+    if (sP === 'BI' && !sI2) {
+      return '[Error]: No Solar Index was given for the second star';
+    }
+
+    // Add Solar Prefix if given
+    if (sP) {
+      name = sP + '.';
+    } else {
+      name = '';
+    }
+
+    // Add Solar Indicies
+    if (sP === 'BI') {
+      name += sI.charAt(0) + sI2.charAt(0);
+    } else {
+      name += sI;
+    }
+    name += '-';
+
+    // Add Planetary Index
+    name += pSize + pClimate + pSurv;
+
+    // Add Planetary Suffixes if given
+    for (var i = 0; i < mods.length; i++) {
+      console.log(mods[i].checked + ':' + mods[i].value);
+      if (mods[i].checked) {
+        name += mods[i].value;
+      }
+    }
+
+    return name;
   }
 
   /**
